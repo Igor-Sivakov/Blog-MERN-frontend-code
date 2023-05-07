502812a94ea2f264fbd40f8930b9bfe3054da890
nomono <sivakovigor.ui@gmail.com>
2023-05-07 15:29:18 +0300

    add registered user file for tests

---
+++ registeredUser.js
@@ -0,0 +1,10 @@
+/* 
+1. Igor Sivakov nomono.eq@gmail.com 123456 
+2. Frank Sinatra sinatra@gmail.com 123456
+3. John Doe doe@gmail.com 123456
+4. Alise Moor moor@gmail.com 123456
+5. Katty Brigs brigs@gmail.com 123456
+6. Robin Shalby shalby@gmail.com 123456
+7. Marlyn Grey grey@gmail.com 123456
+8. Judy Fisher fisher@gmail.com 123456
+*/

4d78b4fbba5d0881406b2bc5fff9d523367898ae
nomono <sivakovigor.ui@gmail.com>
2023-05-07 15:36:39 +0300

    add heroku backend link to instance

--- src/API/instance.ts
+++ src/API/instance.ts
@@ -2,7 +2,7 @@ import axios, { InternalAxiosRequestConfig } from 'axios'
 
 
 export const instance = axios.create({
-  baseURL: 'http://localhost:4444/',
+  baseURL: 'http://localhost:4444/' || 'https://sivakov-blog-mern.herokuapp.com/',
 })
 
 instance.interceptors.request.use((config: InternalAxiosRequestConfig<any>): InternalAxiosRequestConfig<any> | Promise<InternalAxiosRequestConfig<any>> => {
